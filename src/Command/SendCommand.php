8c6feb976e6521a49eb97815dac04dd1f3433f3c
Justin Hoffman <justin@ohmedia.ca>
2022-03-01 14:32:29 -0600

    remove the email sender

--- src/Command/SendCommand.php
+++ src/Command/SendCommand.php
@@ -5,21 +5,22 @@ namespace OHMedia\EmailBundle\Command;
 use DateTime;
 use Doctrine\ORM\EntityManager;
 use Exception;
-use OHMedia\EmailBundle\Entity\Email;
-use OHMedia\EmailBundle\Service\EmailSender;
+use OHMedia\EmailBundle\Entity\Email as EntityEmail;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email as MimeEmail;
 
 class SendCommand extends Command
 {
     private $em;
     private $sender;
 
-    public function __construct(EntityManager $em, EmailSender $sender)
+    public function __construct(EntityManager $em, MailerInterface $mailer)
     {
         $this->em = $em;
-        $this->sender = $sender;
+        $this->mailer = $mailer;
 
         parent::__construct();
     }
@@ -36,15 +37,15 @@ class SendCommand extends Command
     {
         $now = new DateTime();
 
-        $emails = $this->em->getRepository(Email::class)->getUnsent();
+        $entityEmails = $this->em->getRepository(Email::class)->getUnsent();
 
-        foreach ($emails as $email) {
+        foreach ($entityEmails as $entityEmail) {
             // flag the email as sending to prevent double-sending
-            $email->setSending(true);
+            $entityEmail->setSending(true);
             $this->em->flush();
 
             try {
-                $this->sender->send($email);
+                $this->sendEmail($entityEmail);
 
                 // sending was successful if we got here
                 $email->setSentAt($now);
@@ -53,12 +54,39 @@ class SendCommand extends Command
                 // need to catch the exception
                 // but don't need to do anything with it
                 echo $e->getMessage() . "\n";
+
+                // TODO: set an error message on the $entityEmail ?
             }
 
-            $email->setSending(false);
+            $entityEmail->setSending(false);
             $this->em->flush();
         }
 
         return Command::SUCCESS;
+    }
+
+    private function sendEmail(EntityEmail $entityEmail): void
+    {
+        $mimeEmail = new MimeEmail();
+        $mimeEmail
+            ->from(...$entityEmail->getFrom())
+            ->to(...$entityEmail->getTo())
+            ->cc(...$entityEmail->getCc())
+            ->bcc(...$entityEmail->getBcc())
+            ->replyTo(...$entityEmail->getReplyTo())
+            ->subject($entityEmail->getSubject())
+            ->html($entityEmail->getHtml())
+        ;
+
+        foreach ($entityEmail->getAttachments() as $attachment) {
+            $mimeEmail->attachFromPath($attachment->getPath(), $attachment->getName());
+        }
+
+        $mimeEmail->getHeaders()
+            // this header tells auto-repliers ("email holiday mode") to not
+            // reply to this message because it's an automated email
+            ->addTextHeader('X-Auto-Response-Suppress', 'OOF, DR, RN, NRN, AutoReply');
+
+        $this->mailer->send($email);
     }
 }
