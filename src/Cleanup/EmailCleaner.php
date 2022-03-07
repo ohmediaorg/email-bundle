36c84a9dfa3793eb7aef5f19802c41e1931990dc
Justin Hoffman <justin@ohmedia.ca>
2022-03-07 10:28:31 -0600

    update to use the cleanup bundle

---
+++ src/Cleanup/EmailCleaner.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace OHMedia\EmailBundle\Cleanup;
+
+use DateTime;
+use OHMedia\CleanupBundle\Interfaces\CleanerInterface;
+use OHMedia\EmailBundle\Repository\EmailRepository;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class EmailCleaner implements CleanerInterface
+{
+    private $repo;
+    private $cleanup;
+
+    public function __construct(EmailRepository $repo, $cleanup)
+    {
+        $this->repo = $repo;
+        $this->cleanup = $cleanup;
+    }
+
+    public function __invoke(OutputInterface $output): void
+    {
+        $since = new DateTime($this->cleanup);
+
+        $this->repo->deleteSentBefore($since);
+    }
+}
