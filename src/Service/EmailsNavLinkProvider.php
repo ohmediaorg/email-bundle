2c5eabd90cdb00570f8e96fca50bc0cfb36e3735
Justin Hoffman <justin@ohmedia.ca>
2024-05-22 14:51:27 -0600

    developer only nav link

---
+++ src/Service/EmailsNavLinkProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace OHMedia\EmailBundle\Service;
+
+use OHMedia\BackendBundle\Service\AbstractDeveloperOnlyNavLinkProvider;
+use OHMedia\BootstrapBundle\Component\Nav\NavLink;
+use OHMedia\EmailBundle\Entity\Email;
+use OHMedia\EmailBundle\Security\Voter\EmailVoter;
+
+class EmailsNavLinkProvider extends AbstractDeveloperOnlyNavLinkProvider
+{
+    public function getNavLink(): NavLink
+    {
+        return new NavLink('Emails', 'email_index');
+    }
+
+    public function getVoterAttribute(): string
+    {
+        return EmailVoter::INDEX;
+    }
+
+    public function getVoterSubject(): mixed
+    {
+        return new Email();
+    }
+}
